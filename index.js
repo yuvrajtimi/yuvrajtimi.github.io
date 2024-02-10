636d5f1363f64990e67a9f680e9fdda7fb2a121b
TimiRamses <okitahebitimi@gmail.com>
2024-02-10 04:50:03 +0100

    uploaded relevant files

---
+++ index.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+document.querySelector("#serach-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
